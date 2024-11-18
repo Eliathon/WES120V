cbd3247ccf302724dea1b3b26bf401249cc0a747
Sindre <novi.sindre@gmail.com>
2024-11-18 16:43:13 +0100

    Kode fra før

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function setTheme(theme) {
+    if (theme === 'star-wars') {
+        document.body.classList.add('star-wars');
+    } else {
+        document.body.classList.remove('star-wars');
+    }
+}
+
