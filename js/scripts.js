ff66ba99ac99f4219f4120af0e3e35286d2659fe
Sindre <novi.sindre@gmail.com>
2024-11-19 12:30:17 +0100

    Linket alle sidene

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 function setTheme(theme) {
-    if (theme === 'star-wars') {
-        document.body.classList.add('star-wars');
-    } else {
-        document.body.classList.remove('star-wars');
-    }
+  if (theme === "star-wars") {
+    document.body.classList.add("star-wars");
+  } else {
+    document.body.classList.remove("star-wars");
+  }
 }
 
+window.addEventListener("scroll", function () {
+  const parallax = document.querySelector(".hero");
+  let scrollPosition = window.pageYOffset;
+
+  // Adjust the background position
+  parallax.style.backgroundPositionY = scrollPosition * 0.5 + "px";
+});
